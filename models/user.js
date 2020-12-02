230052cb5e83795f6088ceef16ad5699edb9790d
laraibsidd <laraib.sidd7@gmail.com>
2020-12-02 22:53:07 +0530

    udpate

--- models/user.js
+++ models/user.js
@@ -59,7 +59,7 @@ userSchema.methods.generateAuthToken = async function () {
     const user = this
     const token = jwt.sign({
         _id: user._id.toString()
-    }, 'process.env.JWT_SECRET')
+    }, process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({
         token
     })
